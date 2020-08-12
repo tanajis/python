24dc7451cd010421804052c26943d51829f43735
Tanaji Sutar <tanajisutar31@gmail.com>
2020-08-12 13:11:13 +0530

    Update python3_tab2space_indentation.py

--- miscellaneous/python3_tab2space_indentation.py
+++ miscellaneous/python3_tab2space_indentation.py
@@ -1,6 +1,8 @@
 
 # Python 3 does not access mixure of tab and spcae for indentation and prefers only spaces.
 # This program can be used while migration of code from python 2x to 3x
+# 1 tab  = 4 spaces . Replace tab by 4 spaces.
+
 import os
 
 file_path = 'test.py'
